d9d540fc582cbc5976b9bb47627da915394d6c5f
aemaeth0 <39110859+aemaeth0@users.noreply.github.com>
2018-05-20 18:14:58 -0500

    View for MVC (model view controller) of app

--- CourseSectionView.java
+++ CourseSectionView.java
@@ -28,6 +28,9 @@ public class CourseSectionView extends Pane {
 	private Button 				searchButton;
 	private Button 				addButton; 
 	private Button 				removeButton;
+	private Button 				clearButton;
+	private TextField			searchIDField;
+	private TextField			idField;
 	private TextField			a1Field;
 	private TextField			a2Field;
 	private TextField			a3Field;
@@ -35,12 +38,17 @@ public class CourseSectionView extends Pane {
 	private TextField			midtermField;
 	private TextField			finalExamField;
 	private TextField 			finalGradeField;
+	private Label				idLabel;
+	private Label				searchLabel;
 
 	// public methods to allow access to window components
     public ListView<Integer> getList() { return studentIDList; }
     public Button getSearchButton() { return addButton; }
     public Button getAddButton() { return addButton; }
     public Button getRemoveButton() { return removeButton; }
+    public Button getClearButton() { return clearButton; }
+    public TextField getsearchIDField() { return searchIDField; }
+    public TextField getidField() { return idField; }
     public TextField geta1Field() { return a1Field; }
     public TextField geta2Field() { return a2Field; }
     public TextField geta3Field() { return a3Field; }
@@ -55,7 +63,6 @@ public class CourseSectionView extends Pane {
 		
 		// Create and position the StudentID ListView with some students in it
 	    studentIDList = new ListView<Integer>();
-     
 	    studentIDList.setItems(FXCollections.observableArrayList(model.idList(model.getStudents())));
 	    studentIDList.relocate(5, 20);
 	    studentIDList.setPrefSize(550, 250);
@@ -65,105 +72,137 @@ public class CourseSectionView extends Pane {
 	        studentIDListLabel.relocate(5, -5);
 	        studentIDListLabel.setPrefSize(80, 30);	
 	        
+			// Create and position the "idLabel" Label
+	        idLabel = new Label("ID");
+	       idLabel.relocate(55, 300); // 150
+	        idLabel.setPrefSize(80, 30);	 
+	        
 		// Create and position the "a1Label" Label
 	        Label a1Label = new Label("A1");
-	        a1Label.relocate(55, 300);
-	        a1Label.setPrefSize(80, 30);	        
+	        a1Label.relocate(150, 300); // 150
+	        a1Label.setPrefSize(80, 30);	 
+	        
 	    // Create and position the "a2Label" Label
 	        Label a2Label = new Label("A2");
-	        a2Label.relocate(150, 300);
-	        a2Label.setPrefSize(80, 30);	        
+	        a2Label.relocate(245, 300); //245
+	        a2Label.setPrefSize(80, 30);	
+	        
 	    // Create and position the "a3Label" Label
 	        Label a3Label = new Label("A3");
-	        a3Label.relocate(245, 300);
-	        a3Label.setPrefSize(80, 30);	        
+	        a3Label.relocate(340, 300); //340
+	        a3Label.setPrefSize(80, 30);	  
+	        
 	    // Create and position the "a4Label" Label
 	        Label a4Label = new Label("A4");
-	        a4Label.relocate(340, 300);
-	        a4Label.setPrefSize(80, 30);	        
+	        a4Label.relocate(417, 300); //417
+	        a4Label.setPrefSize(80, 30);	    
+	        
 	    // Create and position the "midtermLabel1" Label
 	        Label midtermLabel1 = new Label("Midterm");
-	        midtermLabel1.relocate(417, 285);
-	        midtermLabel1.setPrefSize(80, 30);	        
+	        midtermLabel1.relocate(522, 285); //522
+	        midtermLabel1.setPrefSize(80, 30);
+	        
 	    // Create and position the "midtermLabel2" Label
 	        Label midtermLabel2 = new Label("Exam");
-	        midtermLabel2.relocate(425, 300);
-	        midtermLabel2.setPrefSize(80, 30);	        
+	        midtermLabel2.relocate(520, 300); //520
+	        midtermLabel2.setPrefSize(80, 30);	  
+	        
 	    // Create and position the "finalExamLabel1" Label
 	        Label finalExamLabel1 = new Label("Final");
-	        finalExamLabel1.relocate(522, 285);
+	        finalExamLabel1.relocate(617, 285); //617
 	        finalExamLabel1.setPrefSize(80, 30);	        
 	    // Create and position the "finalExamLabel2" Label
 	        Label finalExamLabel2 = new Label("Exam");
-	        finalExamLabel2.relocate(520, 300);
-	        finalExamLabel2.setPrefSize(80, 30);	        
+	        finalExamLabel2.relocate(615, 300); //615
+	        finalExamLabel2.setPrefSize(80, 30);	    
+	        
 	    // Create and position the "finalGradeLabel1" Label
 	        Label finalGradeLabel1 = new Label("Final");
-	        finalGradeLabel1.relocate(617, 285);
+	        finalGradeLabel1.relocate(712, 285); //710
 	        finalGradeLabel1.setPrefSize(80, 30);	     
+	        
 	    // Create and position the "finalGradeLabel2" Label
 	        Label finalGradeLabel2 = new Label("Grade");
-	        finalGradeLabel2.relocate(615, 300);
-	        finalGradeLabel2.setPrefSize(80, 30);		                  
+	        finalGradeLabel2.relocate(710, 300); //710
+	        finalGradeLabel2.setPrefSize(80, 30);	
+	        
+	     // Create and position the "search" Label
+	        searchLabel = new Label("Search ID:");
+	        searchLabel.relocate(620, -5); // 710
+	        searchLabel.setPrefSize(80, 30);	
+	        
+	        // Create and position the "id" TextField
+	        searchIDField = new TextField();
+	        searchIDField.relocate(570, 20);  //25
+	        searchIDField.setPrefSize(170, 30);	  
+	        
+	     // Create and position the "id" TextField
+	        idField = new TextField();
+	        idField.relocate(25, 325);  //25
+	        idField.setPrefSize(75, 30);	  
+	        
 		// Create and position the "a1" TextField
 	        a1Field = new TextField();
-	        a1Field.relocate(25, 325);
+	        a1Field.relocate(120, 325); //120
 	        a1Field.setPrefSize(75, 30);	        
 	    // Create and position the "a2" TextField
 	        a2Field = new TextField();
-	        a2Field.relocate(120, 325);
+	        a2Field.relocate(215, 325); //215
 	        a2Field.setPrefSize(75, 30);	     
 	    // Create and position the "a3" TextField
 	        a3Field = new TextField();
-	        a3Field.relocate(215, 325);
+	        a3Field.relocate(310, 325); //310
 	        a3Field.setPrefSize(75, 30);	        
 	    // Create and position the "a4" TextField
 	        a4Field = new TextField();
-	        a4Field.relocate(310, 325);
+	        a4Field.relocate(405, 325); //405
 	        a4Field.setPrefSize(75, 30);	        
 	    // Create and position the "midterm" TextField
 	        midtermField = new TextField();
-	        midtermField.relocate(405, 325);
+	        midtermField.relocate(500, 325); //500
 	        midtermField.setPrefSize(75, 30);	        
 	    // Create and position the "finalExam" TextField
 	        finalExamField = new TextField();
-	        finalExamField.relocate(500, 325);
+	        finalExamField.relocate(595, 325); //595
 	        finalExamField.setPrefSize(75, 30);	        
 	    // Create and position the "finalGrade" TextField
 	        finalGradeField = new TextField();
-	        finalGradeField.relocate(595, 325);
-	        finalGradeField.setPrefSize(75, 30);	        
+	        finalGradeField.relocate(690, 325); //690
+	        finalGradeField.setPrefSize(75, 30);
+	        
 		// Create and position the "Search" Button
-		searchButton = new Button("Search");
-        searchButton.relocate(100, 425);
-        searchButton.setPrefSize(100, 25);
+		searchButton = new Button("GO!");
+        searchButton.relocate(740, 20); // y , x
+        searchButton.setPrefSize(50, 30);
         //searchButton.setOnAction(this);        
         // Create and position the "Add" Button
         addButton = new Button("Add");
-        addButton.relocate(300, 425);
+        addButton.relocate(125, 425);
         addButton.setPrefSize(100, 25);
-        //addButton.setOnAction(this);
+       
         // Create and position the "Remove" Button
         removeButton = new Button("Remove");
-        removeButton.relocate(500, 425);
+        removeButton.relocate(325, 425);
         removeButton.setPrefSize(100, 25);	
-        //removeButton.setOnAction(this);
+       
+        // Create and position the "Clear" Button
+        clearButton = new Button("Clear");
+        clearButton.relocate(525, 425);
+        clearButton.setPrefSize(100, 25);	
+        
         
  // Add all the components to the window
-getChildren().addAll(studentIDList,studentIDListLabel,a1Label,a2Label,a3Label,a4Label,midtermLabel1,midtermLabel2,finalExamLabel1,finalExamLabel2,finalGradeLabel1,finalGradeLabel2,a1Field,a2Field,a3Field,a4Field,midtermField,finalExamField,finalGradeField,searchButton,addButton,removeButton);
+getChildren().addAll(studentIDList,studentIDListLabel,idLabel,a1Label,a2Label,a3Label,a4Label,midtermLabel1,midtermLabel2,finalExamLabel1,finalExamLabel2,finalGradeLabel1,finalGradeLabel2,searchLabel,searchIDField,idField,a1Field,a2Field,a3Field,a4Field,midtermField,finalExamField,finalGradeField,searchButton,addButton,removeButton,clearButton);
 
         // Call update() to make sure model contents are shown    
         update();
 	}
 	
 	public void update() {
-		// code for refreshing View
-		/*
-		ArrayList<Student> exactList = new ArrayList<Student>(model.getSize());
-		for (int i=0; i<model.getSize(); i++)
-		exactList[i] = model.getStudents().get(i);
-		studentIDList.setItems(FXCollections.observableArrayList(exactList));
-		*/
-
+		studentIDList.setItems(FXCollections.observableArrayList(model.idList(model.getStudents())));
+		
+		// enable/disable the Add button accordingly
+        addButton.setDisable(idField.getText().trim().length() == 0 && a1Field.getText().trim().length() == 0 && a2Field.getText().trim().length() == 0 && a3Field.getText().trim().length() == 0 && a4Field.getText().trim().length() == 0 && midtermField.getText().trim().length() == 0 && finalExamField.getText().trim().length() == 0 );
+      
 	}
 }
